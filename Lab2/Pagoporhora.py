7c5e24dfa722322e2d7d94f3b0b2c5b6ae8a95b3
Maria Montserrat Mejia Sandoval (24227) <maria.mejia@est.zamorano.edu>
2022-09-12 17:30:29 -0600

    version celcius2farenheit

---
+++ Lab2/Pagoporhora.py
@@ -0,0 +1,3 @@
+horas=int(input("¿Holas trabajadas? "))
+pago=int(input("¿valor por hora?"))
+print("Usted trabajó", horas,"horas y por cada una le pagaron",pago)
---
+++ Lab2/celcius2farenheit.py
@@ -0,0 +1,3 @@
+C=int(input("Cual es la temperatura en grados celsius"))
+F=9/5*C + 32
+print("La temperatura en grados Farenheit es", F, "°")
